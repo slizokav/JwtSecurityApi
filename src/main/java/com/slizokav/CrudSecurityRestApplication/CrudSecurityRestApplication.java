1a693173ecfe4311fe7253524b73c41f718534ae
Dmitriy Slizovsky <slizokav@gmail.com>
2024-04-02 13:34:39 +0300

    added frame

---
+++ src/main/java/com/slizokav/CrudSecurityRestApplication/CrudSecurityRestApplication.java
@@ -0,0 +1,13 @@
+package com.slizokav.CrudSecurityRestApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSecurityRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSecurityRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slizokav/CrudSecurityRestApplication/CrudSecurityRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slizokav.CrudSecurityRestApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSecurityRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
